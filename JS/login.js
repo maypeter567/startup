5fc98fca9d81f7f29a0f745e2fa3b432e661183e
Peter <maypeter0090@gmail.com>
2023-05-25 20:52:24 -0600

    login is now properly recording

--- JS/login.js
+++ JS/login.js
@@ -1,8 +1,8 @@
 function login() {
-    const user_login = document.querySelector("#user_login");
-    const user_password = document.querySelector("#user_password");
-    localStorage.setItem("user_login", user_login.value);
-    localStorage.setItem("user_password", user_password.value);
-    window.location.href = "play.html";
+    const login = document.querySelector("#user_login");
+    const password = document.querySelector("#user_password");
+    localStorage.setItem("user_login", login.value);
+    localStorage.setItem("user_password", password.value);
+    window.location.href = "gamepad.html";
   }
  
