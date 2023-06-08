0e461b7848d09fa1edc81fdde9bedd7f43069a3e
Peter <maypeter0090@gmail.com>
2023-06-07 23:22:12 -0600

    experiments

--- index.js
+++ index.js
@@ -87,3 +87,17 @@ function update_history_internal() {
 function reset() {
   history = [];
 }
+
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('rental');
+
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
