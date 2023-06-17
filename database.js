71ca27fd9afac95214fa4099917ffa9973b59f27
Peter <maypeter0090@gmail.com>
2023-06-17 09:39:09 -0600

    added uuid and bycrpt to database

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 const { MongoClient } = require('mongodb');
+const bycrypt = require('bycrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
 const scoreCollection = db.collection('scores');
+const playerCollection = db.collection('player');
 
 (async function testConnection() {
     await client.connect();
